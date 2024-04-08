76c9bc4ba97302d6b7e0a06f4cde2a4b176e2d2e
StavtroStellar <google-open2018@yandex.ru>
2024-04-08 20:02:44 +0300

    HW 8

--- hw.c
+++ hw.c
@@ -2,47 +2,123 @@
 #include <inttypes.h>
 #include <math.h>
 
-enum {SIZE = 5};
-float average(int * a, int size)
-{
-	int summ = 0;
-	for(int i = 0; i < size; i++)
-	{
-		summ += a[i];
-	}
-	return summ / size;
-}
+
 
 void scanf_in_int_array(int * arr, int size)
 {
-	for (int i = 0; i< size; i++)
+	for (int i = 0; i < size; i++)
 	{
 		scanf("%d", &arr[i]);
 	}
 }
 
-int min_num(int * arr, int size)
+void scanf_in_int_array_unknown_size(int * arr, int *size)
 {
-	int start = arr[0];
-	for(int i = 1; i < size; i++)
+	int i   = 0;
+	
+	while(1)
 	{
-		if(start > arr[i])
-			start = arr[i];
+		if(scanf("%d", &arr[i]) == 1)
+		{
+			i ++;
+		}
+		if(getchar() == '\n'){
+			break;
+		}
 	}
-	return start;
+	*size = i;
 }
+
+void printf_int_array(int * arr, int size)
+{
+	for (int i = 0; i< size; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+}
+
+
+void sort_array(int * a, int size)
+{
+
+	int temp = 1;
+	int num_previous;
+	while(temp){
+		num_previous = a[0];
+		temp = 0;
+		for(int i = 1; i < size; i++)
+		{
+			
+			if(num_previous > a[i])
+			{
+				temp = a[i];
+				a[i] = num_previous;
+				a[i-1] = temp;
+				temp = 1;
+				
+			}
+			else
+			{
+				num_previous = a[i];
+			}
+		}
+	}
+
+}
+
+
+void even_begin(int * a, int size)
+{
+	int i         = 0;
+	int temp      = 0;
+	int temp_size = 0;
+	int temp_arr[size];
+	
+	while (i < size)
+	{
+		if(a[i]%2 != 0)
+		{
+			temp_arr[0] = a[i];
+			temp_size = 0;
+			for(int j = i+1; j < size; j++)
+			{
+				if(a[j]%2==0)
+				{
+					a[i] = a[j];
+					for (int n = 0; n <= temp_size; n++)
+					{
+						a[i+ n + 1] = temp_arr[n];
+					}
+					break;
+				}
+				else
+				{
+					temp_size++;
+					temp_arr[temp_size] = a[j];
+				}
+			}
+			temp++;
+		}
+		i++;
+	}
+
+
+}
+
 
 int main(int argc, char ** argv){
 	int a[5];
+	int size = 5;
+	printf("1. Sort array\n");
+	scanf_in_int_array_unknown_size(a, &size);
+	sort_array(a, size);
+	printf_int_array(a, size);
+	printf("\n");
 	
-	printf("1. Average\n");
-	scanf_in_int_array(a, SIZE);
-	printf("%.3f\n", average(a, SIZE));
-	
-	printf("2. Min num\n");
-	scanf_in_int_array(a, SIZE);
-	printf("%d\n", min_num(a, SIZE));
-	
-	
+	printf("2. Even begin\n");
+	scanf_in_int_array_unknown_size(a, &size);
+	even_begin(a, size);
+	printf_int_array(a, size);
+
 	return 0;
 }

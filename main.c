8a929bd78683da9b1de2a2466e9472a406fd7e36
StavtroStellar <google-open2018@yandex.ru>
2024-04-17 10:45:41 +0300

    HW11

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "sensor.h"
+
+
+int main(int argc, char** argv) {
+    int size = 0;
+    struct sensor data[MAX_LENGTH_MONTH];
+    
+    if (argc < 2) {
+        showHelp();
+        return 1;
+    }
+    printf("\n");
+    
+    char* filename = NULL;
+    int monthFilter = 0;
+
+    for (int i = 1; i < argc; ++i) {
+        if (strcmp(argv[i], "-h") == 0) {
+            showHelp();
+            return 0;
+        }
+        else if (strcmp(argv[i], "-f") == 0 && i + 1 < argc) {
+            filename = argv[i + 1];
+        }
+        else if (strcmp(argv[i], "-m") == 0 && i + 1 < argc) {
+            monthFilter = atoi(argv[i + 1]);
+            if(monthFilter <= 0)
+            {
+                printWarning();
+                printf("Please enter the correct date of the month");
+                stopWarning();
+                return 2;
+            }
+        }
+    }
+    
+    if (filename == NULL) {
+        printWarning();
+        printf("Error: Missing input file argument.\n");
+        stopWarning();
+        return 1;
+    }
+    
+    processCSVFile(&size, data, filename, monthFilter);
+    
+    return 0;
+}
---
+++ makefile
@@ -0,0 +1,15 @@
+all: sensor.exe
+
+sensor.exe: main.o sensor.o
+	gcc -o sensor.exe sensor.o main.o
+
+main.o: main.c
+	gcc -c --std=c99 -o main.o main.c
+
+sensor.o: sensor.c
+	gcc -c --std=c99 -o sensor.o sensor.c
+
+clean:
+	deel *.o
+	del sensor.exe
+
